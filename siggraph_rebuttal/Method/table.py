1a73847a211d2b853cb9358d314c9fd58a05e25d
chLi <565353780@qq.com>
2025-03-11 18:35:18 +0800

    add latex table str support

--- siggraph_rebuttal/Method/table.py
+++ siggraph_rebuttal/Method/table.py
@@ -1,4 +1,33 @@
-def toTableStr(data: list) -> str:
+def toLatexTableStr(data: list) -> str:
+    row_num = len(data)
+    col_num = len(data[0])
+
+    table_str = ''
+
+    table_str += '\\begin{tabular}{'
+
+    for _ in range(col_num):
+        table_str += 'c'
+
+    table_str += '}\n'
+
+    table_str += '\\toprule\n'
+
+    for i in range(row_num):
+        for j in range(col_num - 1):
+            table_str += str(data[i][j])
+            table_str += '&'
+
+        table_str += str(data[i][col_num - 1])
+        table_str += '\\\\\n'
+
+    table_str += '\\bottomrule\n'
+
+    table_str += '\\end{tabular}\n'
+
+    return table_str
+
+def toRebuttalTableStr(data: list) -> str:
     row_num = len(data)
     col_num = len(data[0])
 
@@ -28,3 +57,6 @@ def toTableStr(data: list) -> str:
     table_str += '\\end{array}$$\n'
 
     return table_str
+
+def toTableStr(data: list) -> str:
+    return toLatexTableStr(data) + '\n' + toRebuttalTableStr(data)
