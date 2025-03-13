d7687dff929b678f831832891e7aae4aa1cde461
chLi <565353780@qq.com>
2025-03-13 14:16:41 +0800

    support downarrow, uparrow, bf, underline

--- siggraph_rebuttal/Method/table.py
+++ siggraph_rebuttal/Method/table.py
@@ -27,6 +27,37 @@ def toLatexTableStr(data: list) -> str:
 
     return table_str
 
+def toFormatDataStr(data) -> str:
+    format_data_str = ''
+
+    if '\\' in data:
+        data_list = data.split('\\')
+
+        data_str = data_list[0]
+
+        outer_tag_num = 0
+        for tag in data_list[1:]:
+            if tag in ['bf', 'underline']:
+                format_data_str += '\\' + tag + '{'
+                outer_tag_num += 1
+    else:
+        data_str = data
+
+    format_data_str += '\\text{'
+    format_data_str += str(data_str)
+
+    if '\\' in data:
+        for _ in range(outer_tag_num):
+            format_data_str += '}'
+
+    format_data_str += '}'
+
+    if '\\' in data:
+        for tag in data_list[1:]:
+            if tag in ['downarrow', 'uparrow']:
+                format_data_str += '\\' + tag
+    return format_data_str
+
 def toRebuttalTableStr(data: list) -> str:
     row_num = len(data)
     col_num = len(data[0])
@@ -35,22 +66,18 @@ def toRebuttalTableStr(data: list) -> str:
 
     table_str += '$$\\begin{array}{'
 
-    for _ in range(col_num - 1):
-        table_str += 'c|'
-
-    table_str += 'c'
+    for _ in range(col_num):
+        table_str += 'c'
 
     table_str += '}\\hline\n'
 
     for i in range(row_num):
         for j in range(col_num - 1):
-            table_str += '\\text{'
-            table_str += str(data[i][j])
-            table_str += '}&'
+            table_str += toFormatDataStr(data[i][j])
 
-        table_str += '\\text{'
-        table_str += str(data[i][col_num - 1])
-        table_str += '}'
+            table_str += '&'
+
+        table_str += toFormatDataStr(data[i][col_num - 1])
 
         table_str += '\\\\\\\\\\hline\n'
 
